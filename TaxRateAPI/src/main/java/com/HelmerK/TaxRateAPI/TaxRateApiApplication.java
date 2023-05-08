5ff3e0888363a9ab83b351ee4229c10a607365b0
Kyle-Helmer <99497831+Kyle-Helmer@users.noreply.github.com>
2023-05-08 14:08:05 -0600

    Initial Commit

---
+++ TaxRateAPI/src/main/java/com/HelmerK/TaxRateAPI/TaxRateApiApplication.java
@@ -0,0 +1,13 @@
+package com.HelmerK.TaxRateAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxRateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxRateApiApplication.class, args);
+	}
+
+}
---
+++ TaxRateAPI/src/test/java/com/HelmerK/TaxRateAPI/TaxRateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HelmerK.TaxRateAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxRateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
